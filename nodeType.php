964697bc763bcd783b5c43dca10b98d3e541a7ae
Wes Gonzales <wgonzales@dodgecommunications.com>
2012-10-08 11:26:29 -0400

    Testing commit
    testing here

---
+++ nodeType.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ .Testing git again
+*/
+
+function current_path() {
+	$test = $_GET['q'];
+	echo '$test';
+}
+function name_of_node() {
+  if ($node->type == 'this is where you are looking for the particular node') {
+  echo 'should print something if node type is whatever.'
+  } 
+}
+?>
